34cc1b44dc5c50a81bbd78e5f89badce3391599f
EarthChart0809 <182177669+EarthChart0809@users.noreply.github.com>
2026-02-04 00:07:36 +0900

    Add revalidation and dynamic settings to API routes

--- src/app/api/admin/categories/route.ts
+++ src/app/api/admin/categories/route.ts
@@ -2,6 +2,9 @@ import { prisma } from "@/lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
 import { Category } from "@/generated/prisma/client";
 
+export const revalidate = 0; // ◀ サーバサイドのキャッシュを無効化する設定
+export const dynamic = "force-dynamic"; // ◀ 〃
+
 type RequestBody = {
   name: string;
 };
